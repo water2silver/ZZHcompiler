c23a79e7161f70769b1e0392bf30759cdb73bd58
zenglj <zenglj@live.com>
2024-02-20 22:26:58 +0800

    增加函数原型的说明

--- tests/test1-1.c
+++ tests/test1-1.c
@@ -1,3 +1,7 @@
+// 可追加putint函数的原型，避免静态检查或编译警告的产生
+// 如果不追加原型，编译时需通过--include选型指定头文件忽略警告
+void putint(int);
+
 int main()
 {
     int a, b;
@@ -9,6 +13,6 @@ int main()
     a = 3 + a;
     b = b + 10;
     putint(a + b);
-	
+
     return 0;
 }
